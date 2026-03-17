c3e27545bc6c6427d05a481bea7bda25a16367b7
Diana Vargas <dianavlw@gmail.com>
2026-03-16 20:42:26 -0700

    added chunk function

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import chromadb
 from dotenv import load_dotenv
 from openai import OpenAI
-from pypdf import RdfReader
+from pypdf import PdfReader
 
 load_dotenv()
 #load your variables into the program
@@ -31,3 +31,29 @@ chroma_client=chromadb.Client()
 collection = chroma_client.get_or_create_collection(name="docuchat_chunks")
 
 #Chroma database is where my chatbot will store data, ex. chunks from the PDF
+
+def extract_text_from_pdf(file_path:str) -> str:
+    reader = PdfReader(file_path)
+    full_text = []
+#from pypdf import PdfReader - what this does is loads a tool from the library so we can use it in our code
+#pypdf lets u work with pdf files, it can read, extract text, split, merge pdf and inspect pages
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            full_text.append(page_text)
+    return "\n".join(full_text)
+    
+#funtion takes in three inputs, 800 charactes each chunk, it overlaps to prevent losing context
+#returns an array of chunks, starting at 0 up to 800
+#it ends once it has reached chunk_size
+#with the overlap it prevents from spilling 
+#with overlapping chunks share text that may contain important text retrieval becomes more reliable
+def chunk_text(text:str, chunk_size: int= 800, overlap:int= 150)->list[str]:
+    chunks = []
+    start = 0
+
+    while start <len(text):
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start += chunk_size - overlap
+    return chunks
